a77b676dd790d9e6ff4083093c04ab5059ba863d
Ayla Pearson <ayla@poissonconsulting.ca>
2023-09-07 16:33:15 -0700

    adding in script to read in and generate internal data sets

--- data-raw/data-raw.R
+++ data-raw/data-raw.R
@@ -12,30 +12,24 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# bbourecruit <- tibble::as_tibble(readRDS("data-raw/bbourecruit.rds"))
-# bbousurv <- tibble::as_tibble(readRDS("data-raw/bbbousurv.rds"))
+path <- file.path("data-raw", "recruitment.xlsx")
+sheets <- readxl::excel_sheets(path)
 
-### TODO: Add in sample data
-bbourecruit <- tibble::tibble(
-  PopulationName = c("A"),
-  Year = c(2023L),
-  Month = c(1L),
-  Day = c(1L),
-  Cows = c(10L),
-  Bulls = c(10L),
-  UnknownAdults = c(0L),
-  Yearlings = c(1L),
-  Calves = c(1L)
-)
+bbourecuit_a <- readxl::read_excel(path, sheets[1])
+bbourecuit_b <- readxl::read_excel(path, sheets[2])
+bbourecuit_c <- readxl::read_excel(path, sheets[3])
 
-bbousurv <- tibble::tibble(
-  PopulationName = c("A"),
-  Year = c(2023L),
-  Month = c(1L),
-  StartTotal = c(10L),
-  MortalitiesCertain = c(0L),
-  MortalitiesUncertain = c(0L)
-)
+usethis::use_data(bbourecuit_a, overwrite = TRUE)
+usethis::use_data(bbourecuit_b, overwrite = TRUE)
+usethis::use_data(bbourecuit_c, overwrite = TRUE)
 
-usethis::use_data(bbourecruit, overwrite = TRUE)
-usethis::use_data(bbousurv, overwrite = TRUE)
+path <- file.path("data-raw", "survival.xlsx")
+sheets <- readxl::excel_sheets(path)
+
+bbousurv_a <- readxl::read_excel(path, sheets[1])
+bbousurv_b <- readxl::read_excel(path, sheets[2])
+bbousurv_c <- readxl::read_excel(path, sheets[3])
+
+usethis::use_data(bbousurv_a, overwrite = TRUE)
+usethis::use_data(bbousurv_b, overwrite = TRUE)
+usethis::use_data(bbousurv_c, overwrite = TRUE)
