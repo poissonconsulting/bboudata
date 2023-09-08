54503e2502ac8c00197e20f38cb0dcfa1de34761
Ayla Pearson <ayla@poissonconsulting.ca>
2023-09-07 18:04:19 -0700

    docs

--- R/bbourecruit.R
+++ R/bbourecruit.R
@@ -14,69 +14,72 @@
 
 #' Sample Data for Population A
 #'
-#' The data contains recruitment information for a boreal caribou population B
+#' The data contains recruitment information for boreal caribou population B
 #' which spans 26 years.
 #'
 #' @details The data is released under the [Open Government Licence -
 #' Alberta](https://open.alberta.ca/licence)
 #'
-#' @format A tibble
+#' @format A tibble with columns:
 #' \describe{
 #' \item{PopulationName}{Name of the population}
-#' \item{Year}{The year}
-#' \item{Month}{The month}
-#' \item{Day}{The day}
-#' \item{Cows}{The number of cows}
-#' \item{Bulls}{The number of bulls}
-#' \item{UnknownAdults}{The number of adults which could not be sexed}
-#' \item{Yearlings}{The number of yearlings}
-#' \item{Calves}{The number of calves}
+#' \item{Year}{The year the observation occurred}
+#' \item{Month}{The month the observation occurred}
+#' \item{Day}{The day the observation occurred}
+#' \item{Cows}{The number of cows counted in the group}
+#' \item{Bulls}{The number of bulls counted in the group}
+#' \item{UnknownAdults}{The number of adults which could not be sexed in the
+#' group}
+#' \item{Yearlings}{The number of yearlings counted in the group}
+#' \item{Calves}{The number of calves counted in the group}
 #' }
 #'
 "bbourecruit_a"
 
 #' Sample Data for Population B
 #'
-#' The data contains recruitment information for a boreal caribou population B
+#' The data contains recruitment information for boreal caribou population B
 #' which spans 14 years.
 #'
 #' @details The data is released under the [Open Government Licence -
 #' Alberta](https://open.alberta.ca/licence)
 #'
-#' @format A tibble
+#' @format A tibble with columns:
 #' \describe{
 #' \item{PopulationName}{Name of the population}
-#' \item{Year}{The year}
-#' \item{Month}{The month}
-#' \item{Day}{The day}
-#' \item{Cows}{The number of cows}
-#' \item{Bulls}{The number of bulls}
-#' \item{UnknownAdults}{The number of adults which could not be sexed}
-#' \item{Yearlings}{The number of yearlings}
-#' \item{Calves}{The number of calves}
+#' \item{Year}{The year the observation occurred}
+#' \item{Month}{The month the observation occurred}
+#' \item{Day}{The day the observation occurred}
+#' \item{Cows}{The number of cows counted in the group}
+#' \item{Bulls}{The number of bulls counted in the group}
+#' \item{UnknownAdults}{The number of adults which could not be sexed in the
+#' group}
+#' \item{Yearlings}{The number of yearlings counted in the group}
+#' \item{Calves}{The number of calves counted in the group}
 #' }
 #'
 "bbourecruit_b"
 
 #' Sample Data for Population C
 #'
-#' The data contains recruitment information for a boreal caribou population C
+#' The data contains recruitment information for boreal caribou population C
 #' which spans 8 years.
 #'
 #' @details The data is released under the [Open Government Licence -
 #' Alberta](https://open.alberta.ca/licence)
 #'
-#' @format A tibble
+#' @format A tibble with columns:
 #' \describe{
 #' \item{PopulationName}{Name of the population}
-#' \item{Year}{The year}
-#' \item{Month}{The month}
-#' \item{Day}{The day}
-#' \item{Cows}{The number of cows}
-#' \item{Bulls}{The number of bulls}
-#' \item{UnknownAdults}{The number of adults which could not be sexed}
-#' \item{Yearlings}{The number of yearlings}
-#' \item{Calves}{The number of calves}
+#' \item{Year}{The year the observation occurred}
+#' \item{Month}{The month the observation occurred}
+#' \item{Day}{The day the observation occurred}
+#' \item{Cows}{The number of cows counted in the group}
+#' \item{Bulls}{The number of bulls counted in the group}
+#' \item{UnknownAdults}{The number of adults which could not be sexed in the
+#' group}
+#' \item{Yearlings}{The number of yearlings counted in the group}
+#' \item{Calves}{The number of calves counted in the group}
 #' }
 #'
 "bbourecruit_c"
--- R/bbousurv.R
+++ R/bbousurv.R
@@ -14,69 +14,72 @@
 
 #' Sample Data for Population A
 #'
-#' The data contains survival information for a boreal caribou population A
+#' The data contains survival information for boreal caribou population A
 #' which spans 30 years.
 #'
 #' @details
 #' The data is released under the [Open Government Licence -
 #' Alberta](https://open.alberta.ca/licence)
 #'
-#' @format A tibble
+#' @format A tibble with columns:
 #' \describe{
 #' \item{PopulationName}{Name of the population}
-#' \item{Year}{The year}
-#' \item{Month}{The month}
-#' \item{StartTotal}{The number of living caribou at the start of the month}
+#' \item{Year}{The year the observation occurred}
+#' \item{Month}{The month the observation occurred}
+#' \item{StartTotal}{The total number of collared caribou at the start of the
+#' month}
 #' \item{MortalitiesCertain}{The number of confirmed caribou mortalities in the
 #' month}
-#' \item{MortalitiesUncertain}{The number of unconfirmed caribou mortalities in
-#' the month}
+#' \item{MortalitiesUncertain}{The total number of mortalities that were not
+#' confirmed in that month}
 #' }
 #'
 "bbousurv_a"
 
 #' Sample Data for Population B
 #'
-#' The data contains survival information for a boreal caribou population B
+#' The data contains survival information for boreal caribou population B
 #' which spans 17 years.
 #'
 #' @details
 #' The data is released under the [Open Government Licence -
 #' Alberta](https://open.alberta.ca/licence)
 #'
-#' @format A tibble
+#' @format A tibble with columns:
 #' \describe{
 #' \item{PopulationName}{Name of the population}
-#' \item{Year}{The year}
-#' \item{Month}{The month}
-#' \item{StartTotal}{The number of living caribou at the start of the month}
+#' \item{Year}{The year the observation occurred}
+#' \item{Month}{The month the observation occurred}
+#' \item{StartTotal}{The total number of collared caribou at the start of the
+#' month}
 #' \item{MortalitiesCertain}{The number of confirmed caribou mortalities in the
 #' month}
-#' \item{MortalitiesUncertain}{The number of unconfirmed caribou mortalities in
-#' the month}
+#' \item{MortalitiesUncertain}{The total number of mortalities that were not
+#' confirmed in that month}
 #' }
 #'
 "bbousurv_b"
 
 #' Sample Data for Population C
 #'
-#' The data contains survival information for a boreal caribou population C
+#' The data contains survival information for boreal caribou population C
 #' which spans 10 years.
 #'
 #' @details
 #' The data is released under the [Open Government Licence -
 #' Alberta](https://open.alberta.ca/licence)
 #'
-#' @format A tibble
+#' @format A tibble with columns:
 #' \describe{
 #' \item{PopulationName}{Name of the population}
-#' \item{Year}{The year}
-#' \item{Month}{The month}
-#' \item{StartTotal}{The number of living caribou at the start of the month}
+#' \item{Year}{The year the observation occurred}
+#' \item{Month}{The month the observation occurred}
+#' \item{StartTotal}{The total number of collared caribou at the start of the
+#' month}
 #' \item{MortalitiesCertain}{The number of confirmed caribou mortalities in the
 #' month}
-#' \item{MortalitiesUncertain}{The number of unconfirmed caribou mortalities in
-#' the month}
+#' \item{MortalitiesUncertain}{The total number of mortalities that were not
+#' confirmed in that month}
 #' }
 #'
 "bbousurv_c"
