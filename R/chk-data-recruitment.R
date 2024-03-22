2ca15fc846090e37e053b28bad39340174acfa5a
Ayla Pearson <ayla@poissonconsulting.ca>
2024-03-21 17:28:11 -0700

    adding recruitment data checking function

---
+++ R/chk-data-recruitment.R
@@ -0,0 +1,67 @@
+# Copyright 2024 Province of Alberta
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#' Check recruitment data structure
+#'
+#' @param data A data frame.
+#' @param x_name Name of data frame.
+#'
+#' @return An invisible data frame.
+#' @export
+#'
+#' @examples
+#' bbd_chk_data_recruitment(bbourecruit_a)
+bbd_chk_data_recruitment <- function(data, x_name = deparse(substitute(data))) {
+  nms <- c(
+    "PopulationName", "Year", "Month", "Day", "Cows",
+    "Bulls", "UnknownAdults", "Yearlings", "Calves"
+  )
+  chk::chk_superset(names(data), nms, x_name = x_name)
+
+  chk::chk_character_or_factor(data$PopulationName, x_name = xname(x_name, "PopulationName"))
+  chk::chk_not_any_na(data$PopulationName, x_name = "PopulationName")
+  .chk_population(data)
+
+  chk::chk_whole_numeric(data$Year, x_name = xname(x_name, "Year"))
+  chk::chk_gte(data$Year, 0, x_name = xname(x_name, "Year"))
+
+  chk::chk_whole_numeric(data$Month, x_name = xname(x_name, "Month"))
+  chk::chk_range(data$Month, range = c(1, 12), x_name = xname(x_name, "Month"))
+
+  chk::chk_whole_numeric(data$Day, x_name = xname(x_name, "Day"))
+  chk::chk_range(data$Day, range = c(1, 31), x_name = xname(x_name, "Day"))
+
+  chk::chk_not_any_na(data$Year, x_name = "Year")
+  chk::chk_not_any_na(data$Month, x_name = "Month")
+  chk::chk_not_any_na(data$Day, x_name = "Day")
+
+  .chk_date(data$Year, data$Month, data$Day)
+
+  chk::chk_whole_numeric(data$Cows, x_name = xname(x_name, "Cows"))
+  chk::chk_gte(data$Cows, 0, x_name = xname(x_name, "Cows"))
+
+  chk::chk_whole_numeric(data$Bulls, x_name = xname(x_name, "Bulls"))
+  chk::chk_gte(data$Bulls, 0, x_name = xname(x_name, "Bulls"))
+
+  chk::chk_whole_numeric(data$UnknownAdults, x_name = xname(x_name, "UnknownAdults"))
+  chk::chk_gte(data$UnknownAdults, 0, x_name = xname(x_name, "UnknownAdults"))
+
+  chk::chk_whole_numeric(data$Yearlings, x_name = xname(x_name, "Yearlings"))
+  chk::chk_gte(data$Yearlings, 0, x_name = xname(x_name, "Yearlings"))
+
+  chk::chk_whole_numeric(data$Calves, x_name = xname(x_name, "Calves"))
+  chk::chk_gte(data$Calves, 0, x_name = xname(x_name, "Calves"))
+
+  invisible(data)
+}
--- R/functions.R
+++ R/functions.R
@@ -1,3 +1,17 @@
+# Copyright 2024 Province of Alberta
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 .chk_population <- function(x) {
   if (.vld_population(x)) {
     return(invisible())
@@ -29,4 +43,20 @@
 
 xname <- function(x, col) {
   paste("Column", col, "of", x)
+}
+
+.chk_date <- function(year, month, day) {
+  chars <- paste(year, month, day, sep = "-")
+  dates <- as.Date(chars)
+  na_dates <- chars[is.na(dates)]
+  if (!length(na_dates)) {
+    return(invisible())
+  }
+  chk::abort_chk(
+    paste(
+      "Dates",
+      chk::cc(na_dates, ' and '),
+      "are invalid."
+    )
+  )
 }
